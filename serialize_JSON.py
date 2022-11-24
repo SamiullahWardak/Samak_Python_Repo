d6cea47cc42ff797f25cf3519b316d770bf589dd
Samiullah Wardak <samakwardak777@gmail.com>
2022-11-24 22:17:22 +0430

    New syntax added to JSON files!

--- serialize_JSON.py
+++ serialize_JSON.py
@@ -6,12 +6,16 @@ student = {
     'Roll_no':'00912992',
     'Grade':'A',
     'Age':'22',
+    'Gender': 'Male',
     'Subject':['Software Engineering', 'Graphic Designing', 'Data Structure']
 }
 
-with open('data.json', 'w') as write_file:
-    json.dump(student, write_file)
-    write_file.close()
+# with open('Student_Data.json', 'w') as write_file:
+#     json.dump(student, write_file)
+#     write_file.close()
+file = open('Student_Data.json', 'w')
+json.dump(student, file)
+file.close()
 
 b = json.dumps(student)
 print(b)
